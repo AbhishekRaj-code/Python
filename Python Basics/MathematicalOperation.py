c4552ec2f0ce069213579c874dd6620c44bdc7b0
AbhishekRaj-code <rajabhishek4507@gmail.com>
2025-12-04 15:34:38 +0530

    Error Handling

--- Python Basics/MathematicalOperation.py
+++ Python Basics/MathematicalOperation.py
@@ -6,8 +6,7 @@ num2 = int(input("Enter second number: "))
 addition = num1 + num2  
 subtraction = num1 - num2
 multiplication = num1 * num2
-division = num1 / num2 if num2 != 0 else "Undefined (cannot divide by zero)"
-division = round(division,2)
+division = round(num1 / num2,2) if num2 != 0 else print("Error: Division by zero is not allowed.")
 
 # Displaying the results
 print(f"Addition: {num1} + {num2} = {addition}")
