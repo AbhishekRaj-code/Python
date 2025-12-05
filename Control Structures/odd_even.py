e3f33a5c65a35feb685f507249cff356e0286b03
AbhishekRaj-code <rajabhishek4507@gmail.com>
2025-12-05 15:05:38 +0530

    Understanding Control Structures

---
+++ Control Structures/odd_even.py
@@ -0,0 +1,8 @@
+#Taking input from the user
+num = int(input("Enter an integer: "))
+
+#Checking if the number is odd or even
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
---
+++ Control Structures/sum_of_int.py
@@ -0,0 +1,5 @@
+sum = 0
+#Calculating the sum of integers from 1 to 50
+for num in range(1, 51):
+    sum += num
+print("The sum of integers from 1 to 50 is:", sum)
